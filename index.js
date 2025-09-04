8e351d8188937f771ad168814237d855dc740a68
varunkr63 <153918985+Varunkr63@users.noreply.github.com>
2025-09-05 01:40:01 +0530

    javascript basics

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ y = 6;
 let z = x + y;
 console.log(z);
 
-console.log(  typeof z);
+console.log(typeof z);
+
+const account_id=123456
+let accountName = "varun"
+var accountPassword = "varun123"
+accountCity= "delhi"
+let accountState;
+
+
+// prefer not to use var,use let or const instead because var is function 
+// scoped and can lead to unexpected behavior,issue in block scope and functional scope
+
+console.table([account_id, accountName, accountPassword , accountCity, accountState]);
+
 

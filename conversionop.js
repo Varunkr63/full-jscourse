9e9af7f164abe2fc6b46188393dec61575dce420
varunkr63 <153918985+Varunkr63@users.noreply.github.com>
2025-09-05 17:00:08 +0530

    conversion in js

---
+++ conversionop.js
@@ -0,0 +1,31 @@
+let score = "33"
+ 
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" = number
+// "33abc"= NaN
+// null = 0
+//undefined = NaN
+//true = 1 /false = 0
+// 
+let isLoggedIn = "varun"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+
+
+// 1= true
+// 0 = false
+// "varun" = true
+// "" = false
+
+let someValue= 33
+let stringValue = String(someValue);
+console.log(stringValue);
+console.log(typeof stringValue);
+
